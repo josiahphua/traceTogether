e1a81cd1dc06e18a72bc3be07a0885263a79be4f
josiah <josiahphuajy@gmail.com>
2023-04-19 22:26:34 +0800

    SQL script and docker-compose update

--- src/main/kotlin/com/TraceTogether/Lai/model/ToDo.kt
+++ src/main/kotlin/com/TraceTogether/Lai/model/ToDo.kt
@@ -1,46 +1,46 @@
 package com.TraceTogether.Lai.model
 
-//import jakarta.persistence.Column
+import jakarta.persistence.Column
+import jakarta.persistence.Table
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Entity
-//@Table(name = "todo")
+@Table(name = "todo")
 data class ToDo(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
 
-//        @Column(name = "title")
+        @Column(name = "title")
         var title: String,
 
-//        @Column(name = "status")
+        @Column(name = "status")
         var status: String,
 
-//        @Column(name = "created_at")
+        @Column(name = "created_at")
         var createdAt: LocalDateTime = LocalDateTime.now(),
 
-//        @Column(name = "todo_start_date")
+        @Column(name = "todo_start_date")
         var todoStartDate: LocalDate? = LocalDate.now(),
 
-//        @Column(name = "todo_end_date")
+        @Column(name = "todo_end_date")
         var todoEndDate: LocalDate? = LocalDate.now(),
 
-//        @Column(name = "description")
+        @Column(name = "description")
         var description: String? = null,
 
-//        @Column(name = "email")
+        @Column(name = "email")
         var email: String? = null,
 
-//        @Column(name = "created_by")
+        @Column(name = "created_by")
         var createdBy: String? = null,
 
-//        @Column(name = "priority")
+        @Column(name = "priority")
         var priority: String? = null
 )
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE trace_together IF NOT
+CREATE DATABASE IF NOT EXISTS todo (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  status VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  todo_start_date DATE NOT NULL,
+  todo_end_date DATE NOT NULL,
+  description VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  created_by VARCHAR(255) NOT NULL,
+  priority INTEGER NOT NULL
+)

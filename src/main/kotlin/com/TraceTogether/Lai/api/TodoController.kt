1047a3c1a6d829acfdd3e725c1e54f5ef2a24096
josiah <josiahphuajy@gmail.com>
2023-04-30 14:18:34 +0800

    resolve CORS issue

--- src/main/kotlin/com/TraceTogether/Lai/api/TodoController.kt
+++ src/main/kotlin/com/TraceTogether/Lai/api/TodoController.kt
@@ -2,14 +2,7 @@ package com.TraceTogether.Lai.api
 
 import com.TraceTogether.Lai.dto.TodoDto
 import com.TraceTogether.Lai.service.TodoService
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/todos")
@@ -17,6 +10,7 @@ class TodoController(
     val todoService: TodoService
 ) {
   @GetMapping
+  @CrossOrigin(origins = arrayOf("*"))
   fun getAllTodos(): List<TodoDto> {
     return todoService.getAllTodos()
   }
